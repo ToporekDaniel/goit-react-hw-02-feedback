447aeb0074d781d937d81fca7b9f8b5d71ac6795
ToporekDaniel <danieltoporek0@gmail.com>
2024-01-21 15:07:07 +0100

    something work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import React, { useState } from 'react';
+import { FBtn } from './button/button';
+
 export const App = () => {
+  const [feedback, setFeedback] = useState({
+    Good: 0,
+    Neutral: 0,
+    Bad: 0,
+  });
+
+  const handleBtnClick = btnName => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [btnName]: prevFeedback[btnName] + 1,
+    }));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      test działania
-    </div>
+    <>
+      <FBtn btnName="Good" onClick={() => handleBtnClick('Good')} />
+      <FBtn btnName="Neutral" onClick={() => handleBtnClick('Neutral')} />
+      <FBtn btnName="Bad" onClick={() => handleBtnClick('Bad')} />
+      <div>
+        Good: {feedback.Good}, Neutral: {feedback.Neutral}, Bad: {feedback.Bad}
+      </div>
+    </>
   );
 };
